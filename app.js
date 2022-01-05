3f4a4963199c0f478898ba2fbfeef01cd92b70da
Alexander Mamani <alex@MacBook-Air-de-Alexander.local>
2022-01-05 19:22:04 -0300

    mvc 11

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const mainRoute = require('./routers/main')
+const port = 3000;
+
+// MiddleWare
+app.use(express.static('public'));
+
+// Rutas
+app.use('/', mainRoute);
+
+//Server MSG
+app.listen(port, ()=>{
+    console.log('Servidor funcionando en el puerto : '+port);
+});
